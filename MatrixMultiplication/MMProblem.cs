305d5605ca2aa09e2cd9ddbbc388eff84ef54ca3
Saif samer <91379641+Lord-Biko-1@users.noreply.github.com>
2023-12-20 16:36:27 +0200

    Add files via upload

---
+++ MatrixMultiplication/MMProblem.cs
@@ -0,0 +1,265 @@
+﻿using Helpers;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Problem
+{
+
+    public class Problem : ProblemBase, IProblem
+    {
+        #region ProblemBase Methods
+        public override string ProblemName { get { return "MatrixMultiplication"; } }
+
+        public override void TryMyCode()
+        {
+            int N = 0 ;
+            int[,] output;
+            
+            N = 2;
+            int[,] M1 = { { 1, 1 }, { 1, 0 } };
+            int[,] M2 = { { 1, 1 }, { 1, 0 } };
+
+            int[,] expected = { { 2, 1 }, { 1, 1 } };
+            output = MatrixMultiplication.MatrixMultiply(M1, M2, N);
+            PrintCase(N, M1, M2, output, expected);
+
+            N = 4;
+            int[,] K1 = { { 1, -1, 1, -1 }, { 1, -1, 1, -1 }, { 1, -1, 1, -1 }, { 1, -1, 1, -1 } };
+            int[,] K2 = { { -1, 1, -1, 1 }, { -1, 1, -1, 1 }, { -1, 1, -1, 1 }, { -1, 1, -1, 1 } };
+
+            int[,] expected2 = { { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }};
+            output = MatrixMultiplication.MatrixMultiply(K1, K2, N);
+            PrintCase(N, K1, K2, output, expected2);
+
+            N = 4;
+            int[,] J1 = { { 1, -1, 1, -1 }, { 1, -1, 1, -1 }, { 1, -1, 1, -1 }, { 1, -1, 1, -1 } };
+            int[,] J2 = { { 1, -1, 1, -1 }, { -1, 1, -1, 1 }, { 1, -1, 1, -1 }, { -1, 1, -1, 1 } };
+
+            int[,] expected3 = { { 4, -4, 4, -4 }, { 4, -4, 4, -4 }, { 4, -4, 4, -4 }, { 4, -4, 4, -4 }};
+            output = MatrixMultiplication.MatrixMultiply(J1, J2, N);
+            PrintCase(N, J1, J2, output, expected3);
+
+
+            N = 8;
+            int[,] L1 = { { 1, -2, 3, -4, 5, -6, 7, -8 }, 
+                            { -1, 2, -3, 4, -5, 6, -7, 8 },
+                            { 1, -2, 3, -4, 5, -6, 7, -8 }, 
+                            { -1, 2, -3, 4, -5, 6, -7, 8 },
+                            { 1, -2, 3, -4, 5, -6, 7, -8 }, 
+                            { -1, 2, -3, 4, -5, 6, -7, 8 },
+                            { 1, -2, 3, -4, 5, -6, 7, -8 }, 
+                            { -1, 2, -3, 4, -5, 6, -7, 8 }};
+            int[,] L2 = {   { 1, 0, 0, 0, 0, 0, 0, 0 }, 
+                              { 0, 1, 0, 0, 0, 0, 0, 0 }, 
+                              { 0, 0, 1, 0, 0, 0, 0, 0 }, 
+                              { 0, 0, 0, 1, 0, 0, 0, 0 }, 
+                              { 0, 0, 0, 0, 1, 0, 0, 0 }, 
+                              { 0, 0, 0, 0, 0, 1, 0, 0 }, 
+                              { 0, 0, 0, 0, 0, 0, 1, 0 }, 
+                              { 0, 0, 0, 0, 0, 0, 0, 1 }};
+
+            int[,] expected4 = {{ 1, -2, 3, -4, 5, -6, 7, -8 }, 
+                                { -1, 2, -3, 4, -5, 6, -7, 8 },
+                                { 1, -2, 3, -4, 5, -6, 7, -8 }, 
+                                { -1, 2, -3, 4, -5, 6, -7, 8 },
+                                { 1, -2, 3, -4, 5, -6, 7, -8 }, 
+                                { -1, 2, -3, 4, -5, 6, -7, 8 },
+                                { 1, -2, 3, -4, 5, -6, 7, -8 }, 
+                                { -1, 2, -3, 4, -5, 6, -7, 8 }};
+            output = MatrixMultiplication.MatrixMultiply(L1, L2, N);
+            PrintCase(N, L1, L2, output, expected4);
+        }
+
+ 
+        Thread tstCaseThr;
+        bool caseTimedOut ;
+        bool caseException;
+
+        protected override void RunOnSpecificFile(string fileName, HardniessLevel level, int timeOutInMillisec)
+        {
+            int testCases;
+            int N = 0;
+            int[,] output = null;
+            int[,] actualResult = null;
+            int x=0, y=0;
+
+            Stream s = new FileStream(fileName, FileMode.Open);
+            BinaryReader br = new BinaryReader(s);
+
+            testCases = br.ReadInt32();
+
+            int totalCases = testCases;
+            int correctCases = 0;
+            int wrongCases = 0;
+            int timeLimitCases = 0;
+
+            int i = 1;
+            while (testCases-- > 0)
+            {
+                N = br.ReadInt32();
+                int[,] M1 = new int[N, N];
+                int[,] M2 = new int[N, N];
+                actualResult = new int[N, N];
+
+                for (y = 0; y < N; y++)
+                {
+                    for (x = 0; x < N; x++)
+                    {
+                        M1[y, x] = br.ReadSByte();
+                    }
+                }
+                for (y = 0; y < N; y++)
+                {
+                    for (x = 0; x < N; x++)
+                    {
+                        M2[y, x] = br.ReadSByte();
+                    }
+                }
+                for ( y = 0; y < N; y++)
+                {
+                    for ( x = 0; x < N; x++)
+                    {
+                        actualResult[y, x] = br.ReadInt32();
+                    }
+                }
+                caseTimedOut = true;
+                caseException = false;
+                {
+                    tstCaseThr = new Thread(() =>
+                    {
+                        try
+                        {
+                            Stopwatch sw = Stopwatch.StartNew();
+                            output = MatrixMultiplication.MatrixMultiply(M1, M2, N);
+                            sw.Stop();
+                            Console.WriteLine("N = {0}, time in sec = {1}", N, sw.ElapsedMilliseconds / 1000.0);
+                        }
+                        catch
+                        {
+                            caseException = true;
+                            output = null;
+                        }
+                        caseTimedOut = false;
+                    });
+
+                    //StartTimer(timeOutInMillisec);
+                    tstCaseThr.Start();
+                    tstCaseThr.Join(timeOutInMillisec);
+                }
+                if (caseTimedOut)       //Timedout
+                {
+                    Console.WriteLine("Time Limit Exceeded in Case {0}.", i);
+                    tstCaseThr.Abort();
+                    timeLimitCases++;
+                }
+                else if (caseException) //Exception 
+                {
+                    Console.WriteLine("Exception in Case {0}.", i);
+                    wrongCases++;
+                }
+                else if (output != null && CheckOutput(output, actualResult, ref y, ref x))    //Passed
+                {
+                    Console.WriteLine("Test Case {0} Passed!", i);
+                    correctCases++;
+                }
+                else                    //WrongAnswer
+                {
+                    Console.WriteLine("Wrong Answer in Case {0}.", i);
+                    if (level == HardniessLevel.Easy)
+                    {
+                        PrintCase(N, M1, M2, output, actualResult);
+                    }
+                    wrongCases++;
+                }
+
+                i++;
+            }
+            s.Close();
+            br.Close();
+            Console.WriteLine();
+            Console.WriteLine("# correct = {0}", correctCases);
+            Console.WriteLine("# time limit = {0}", timeLimitCases);
+            Console.WriteLine("# wrong = {0}", wrongCases);
+            Console.WriteLine("\nFINAL EVALUATION (%) = {0}", Math.Round((float)correctCases / totalCases * 100, 0)); 
+        }
+
+       
+
+        protected override void OnTimeOut(DateTime signalTime)
+        {
+        }
+
+        public override void GenerateTestCases(HardniessLevel level, int numOfCases)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Helper Methods
+        private static void PrintMatrix(int[,] matrix)
+        {
+
+            int N = matrix.GetLength(0);
+            int M = matrix.GetLength(1);
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    Console.Write(matrix[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private void PrintCase(int N, int[,] M1, int[,] M2, int[,] output, int[,] expected)
+        {
+            int x=0, y=0;
+            Console.WriteLine("N = {0}", N);
+            Console.WriteLine("Matrix#1 = "); PrintMatrix(M1);
+            Console.WriteLine("Matrix#2 = "); PrintMatrix(M2);
+            Console.WriteLine("output = "); if (output != null) PrintMatrix(output); else Console.WriteLine("NULL");
+            Console.WriteLine("expected = "); PrintMatrix(expected);
+            if (output != null)
+            {
+                if (CheckOutput(output, expected, ref y, ref x))
+                {
+                    Console.WriteLine("CORRECT");
+                }
+                else
+                {
+                    Console.WriteLine("WRONG: output[{0},{1}] = {2} , expected[{0},{1}] = {3}", y, x, output[y, x], expected[y, x]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("output is NULL!");
+            }
+            Console.WriteLine();
+        }
+
+        private bool CheckOutput(int[,] output, int[,] actualResult, ref int y, ref int x)
+        {
+            int N = output.GetLength(0);
+            for (y = 0; y < N; y++)
+            {
+                for (x = 0; x < N; x++)
+                {
+                    if (output[y, x] != actualResult[y, x]) return false;
+                }
+            }
+            return true;
+        }
+        #endregion
+   
+    }
+}
---
+++ MatrixMultiplication/MatrixMultiplication.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem
+{
+    // *****************************************
+    // DON'T CHANGE CLASS OR FUNCTION NAME
+    // YOU CAN ADD FUNCTIONS IF YOU NEED TO
+    // *****************************************
+    public static class MatrixMultiplication
+    {
+        #region YOUR CODE IS HERE
+        
+        //Your Code is Here:
+        //==================
+        /// <summary>
+        /// Multiply 2 square matrices in an efficient way [Strassen's Method]
+        /// </summary>
+        /// <param name="M1">First square matrix</param>
+        /// <param name="M2">Second square matrix</param>
+        /// <param name="N">Dimension (power of 2)</param>
+        /// <returns>Resulting square matrix</returns>
+        static public int[,] MatrixMultiply(int[,] M1, int[,] M2, int N)
+        {
+            //REMOVE THIS LINE BEFORE START CODING
+            int[,] finalMatrix = new int[N, N];
+            int nos = N/2;
+
+
+            //base case 
+            //N<=40 =>10.19
+            //N<=32 =>9.752
+            //N<=35 => 9.632
+            //N<=38 =>9.746
+            //N<=64 =>7.65 best 
+            if (N<=64)
+            {
+                finalMatrix = NaiveMultiplecation(M1, M2, N);
+                return finalMatrix;
+            }
+            //matrix one
+            int[,] mat1_a_00 = new int[nos, nos];
+            int[,] mat1_b_01 = new int[nos, nos];
+            int[,] mat1_c_10 = new int[nos, nos];
+            int[,] mat1_d_11 = new int[nos, nos];
+            //mmatrix2
+            int[,] mat2_e_00 = new int[nos, nos];
+            int[,] mat2_f_01 = new int[nos, nos];
+            int[,] mat2_g_10 = new int[nos, nos];
+            int[,] mat2_h_11 = new int[nos, nos];
+
+
+            //prepare filling matrix intialized with params to use in strassen equation 
+            //mat1=>M1
+            //mat2=>M2
+            //O(N^2/4)=>(N^2)
+
+            //firstmatrix
+            Parallel.For(0, nos, new ParallelOptions { MaxDegreeOfParallelism = 4 },rows =>
+            {for (int col = 0; col < nos; col++)
+                {
+                    //first mat
+                    mat1_a_00[rows, col] = M1[rows, col];
+                    mat1_b_01[rows, col] = M1[rows, col + nos];
+                    mat1_c_10[rows, col] = M1[rows + nos, col];
+                    mat1_d_11[rows, col] = M1[rows + nos, col + nos];
+
+                }
+            });
+            //secound matrix
+            Parallel.For(0, nos, new ParallelOptions { MaxDegreeOfParallelism= 4 },rows =>{
+
+                for (int col = 0; col < nos; col++)
+                {
+                    //second mat
+                    mat2_e_00[rows, col] = M2[rows, col];
+                    mat2_f_01[rows, col] = M2[rows, col + nos];
+                    mat2_g_10[rows, col] = M2[rows + nos, col];
+                    mat2_h_11[rows, col] = M2[rows + nos, col + nos];
+                }
+            });
+
+
+            //getting 7 parts 
+
+            Task<int[,]> part1 = Task.Factory.StartNew(() => MatrixMultiply(mat1_a_00, DiffOrAdd(mat2_f_01, mat2_h_11, nos, false), nos));
+            
+            Task<int[,]> part2 = Task.Factory.StartNew(() => MatrixMultiply(DiffOrAdd(mat1_a_00, mat1_b_01, nos, true), mat2_h_11, nos));
+           
+            Task<int[,]> part3 = Task.Factory.StartNew(() => MatrixMultiply(DiffOrAdd(mat1_c_10, mat1_d_11, nos, true), mat2_e_00, nos));
+            
+            Task<int[,]> part4 = Task.Factory.StartNew(() => MatrixMultiply(mat1_d_11, DiffOrAdd(mat2_g_10, mat2_e_00, nos, false), nos));
+           
+            Task<int[,]> part5 = Task.Factory.StartNew(() => MatrixMultiply(DiffOrAdd(mat1_a_00, mat1_d_11, N / 2, true), DiffOrAdd(mat2_e_00, mat2_h_11, nos, true), nos));
+            
+            Task<int[,]> part6 = Task.Factory.StartNew(() => MatrixMultiply(DiffOrAdd(mat1_b_01, mat1_d_11, nos, false), DiffOrAdd(mat2_g_10, mat2_h_11, nos, true), nos));
+            
+            Task<int[,]> part7 = Task.Factory.StartNew(() => MatrixMultiply(DiffOrAdd(mat1_a_00, mat1_c_10, nos, false), DiffOrAdd(mat2_e_00, mat2_f_01, nos, true), nos));
+            Task.WaitAll(part1, part2, part3, part4, part5, part6, part7);
+            /*
+            int[,] X_00 = DiffOrAdd(DiffOrAdd(part5.Result, part6.Result, nos,true), DiffOrAdd(part4.Result, part2.Result, nos,false), nos,true);
+            int[,] X_01 = DiffOrAdd(part1.Result, part2.Result,nos,true);
+            int[,] X_10 = DiffOrAdd(part3.Result, part4.Result,nos,true);
+            int[,] X_11 = DiffOrAdd(DiffOrAdd(part5.Result, part7.Result, nos,false), DiffOrAdd(part1.Result, part3.Result, nos,false), nos,true);
+            
+
+             for (int i=0;i<nos;i++)
+             {
+                 for(int j=0;j<nos;j++)
+                 {
+
+                     finalMatrix[i, j] = X_00[i,j];
+                     finalMatrix[i, j + nos] = X_01[i,j];
+                     finalMatrix[i + nos, j] = X_10[i, j];
+                     finalMatrix[i + nos, j + nos] = X_11[i, j];
+                 }
+             }*/
+
+            Parallel.For(0, nos,i => {
+
+                for (int j = 0; j < nos; j++)
+                {
+                    finalMatrix[i, j] = part5.Result[i, j] + part4.Result[i, j] - part2.Result[i, j] + part6.Result[i, j];
+                    finalMatrix[i, j + nos] = part1.Result[i, j] + part2.Result[i, j];
+                    finalMatrix[i +nos, j] =part3.Result[i, j] + part4.Result[i, j];
+                    finalMatrix[i + nos, j + nos] = part5.Result[i, j] + part1.Result[i, j] - part3.Result[i, j] - part7.Result[i, j];
+                }
+            });
+
+            return finalMatrix;
+
+        }
+        static public int[,] DiffOrAdd(int[,] mat1, int[,] mat2,int N,bool add)
+        {
+            int[,] result = new int[N, N];
+            if(N==1)
+            {
+                if(add)
+                {
+                    result[0, 0] = mat1[0, 0] + mat2[0, 0];
+                    return result;
+                }
+                else
+                {
+                    result[0, 0] = mat1[0, 0] - mat2[0, 0];
+                    return result;
+                }
+            }
+            //Time=>8.925
+             for(int i=0;i<N;i++)
+              {
+                  for(int j=0;j<N;j++)
+                  {
+                      if(add)
+                      {
+                          result[i, j] = mat1[i, j] + mat2[i, j];
+                      }
+                      else
+                      {
+                          result[i, j] = mat1[i, j] - mat2[i, j];
+                      }
+                  }
+              }
+            //time=>9.893
+            /*Parallel.For(0, N / 2, i => {
+                for (int j = 0; j < N; j++)
+                {
+                    if (add)
+                    {
+                        result[i, j] = mat1[i, j] + mat2[i, j];
+                    }
+                    else
+                    {
+                        result[i, j] = mat1[i, j] - mat2[i, j];
+                    }
+                }
+            });*/
+
+
+            return result;
+
+        }
+        static public int[,] NaiveMultiplecation(int[,]mat1,int[,]mat2,int N)   
+        {
+            int[,] Result = new int[N, N];
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    for (int k = 0; k < N; k++)
+                    {
+                        Result[i, j] += mat1[i, k] * mat2[k, j];
+                    }
+                }
+            }
+            return Result;
+
+        }
+
+        #endregion
+    }
+}
